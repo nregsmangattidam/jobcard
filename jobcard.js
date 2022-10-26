3b07fcc5ca225a600d2a0b747c157e186bc6e29b
nregsmangattidam <111747229+nregsmangattidam@users.noreply.github.com>
2022-10-26 09:00:22 +0100

    Add files via upload

--- jobcard.js
+++ jobcard.js
@@ -1,6 +1,6 @@
 function jobCard() {
-    let n = prompt("വാർഡ് നമ്പർ ?");
-    let m = prompt("തൊഴിൽകാർഡ് നമ്പർ ?")
+    let n = document.getElementById("ward").value;
+    let m = document.getElementById("jobcard").value
     if (n<10) {
         window.location.href  = "https://mnregaweb2.nic.in/netnrega/state_html/jcr.aspx?reg_no=KL-02-006-004-00"+n+"%2f"+m+"&state_name=&district_name=&block_name=&panchayat_name=&village_code=1602006004017+++++&fin_year=&panchayat_code=1602006004&Digest=DXLDauVcnWvIAT6moKBidg";
     } else {
